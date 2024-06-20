c1ce7ba882eae73c99ab420f2cb6451e19b23ad0
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-20 15:37:40 +0530

    refactor: Update IntroRoute.js to use new question-answering functionality

--- server/controllers/IntroRoute.js
+++ server/controllers/IntroRoute.js
@@ -4,25 +4,9 @@ const { generateContent } = require('../utils/gemini_model');
 const router = express.Router();
 const answerPrompt = "Rate the answer on a scale of 1-10, don't defend your answer, just rate it:";
 
-async function askQuestion(question, answer) {
-    const response = await generateContent(answerPrompt, question, answer);
-    const finRes = response.response.text().split(' ')[0];
-    return parseInt(finRes);
-}
 
-async function askQuestions(questions, answers) {
-    const ratings = [];
-    for (let i = 0; i < questions.length; i++) {
-        const rating = await askQuestion(questions[i], answers[i]);
-        ratings.push(rating);
-    }
-    return Promise.all(ratings);
-}
-
-router.post('/', async (req, res) => {
-    const { question, answer } = req.body;
-    const rating = await askQuestion(question, answer);
-    res.json({ rating });
+router.get('/', (req, res) => {
+    res.send('Intro Route');
 });
 
 module.exports = router;
