65effd8904d6f9621c049e349e4b31edc2e6f167
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-20 15:43:15 +0530

    modified intro route

--- server/controllers/userRoute.js
+++ server/controllers/userRoute.js
@@ -10,4 +10,57 @@ router.get('/', (req, res) => {
     res.send('Intro Route');
 });
 
+
+// Multer storage configuration
+const upload = multer({ storage: multer.memoryStorage() });
+
+// POST route to upload file and user data
+router.post('/', upload.single('resume'), async (req, res) => {
+    try {
+        const { email, phone, fullName, linkedin } = req.body;
+        const dataBuffer = req.file.buffer;
+        if (!linkedin && !req.file.buffer) {
+            throw new Error('Please provide either LinkedIn or resume');
+        }
+        let technologies;
+        // return console.log(email, phone, fullName, linkedin);
+
+        if (dataBuffer) {
+            const data = await pdfParse(dataBuffer);
+            const text = data.text;
+            const response = await generateContent(prompt, text);
+            technologies = response.response.text();
+            let start;
+            let end;
+            for (let i = 0; i < technologies.length; i++) {
+                if (technologies[i] == '[') start = i + 1;
+                if (technologies[i] == ']') end = i;
+            }
+            technologies = technologies.substring(start, end);
+        }
+
+        const user = new User({
+            email,
+            phone,
+            fullName,
+            linkedin,
+            technologies,
+            resume: dataBuffer,
+            resumeContentType: req.file.mimetype
+        });
+
+        await user.save();
+        res.status(201).json({
+            _id: user._id,
+            message: 'User created successfully',
+            name: user.fullName,
+            email: user.email,
+            technologies
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+});
+
 module.exports = router;
