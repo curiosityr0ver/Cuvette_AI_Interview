f30f36ccd1aa5e8b86c3a81267fd2f14e5a551b5
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-20 13:09:25 +0530

    chore: Update server configuration and routes
    This commit updates the server configuration and routes in the `index.js` file. It removes the 'dev' script from the `package.json` file, updates the start script to use 'node' instead of 'nodemon', and updates the route for the PDF extractor. Additionally, it updates the landing page styles to improve the layout and visual appearance. Please review and merge the changes as needed.

--- server/index.js
+++ server/index.js
@@ -8,17 +8,9 @@ const quizRoute = require('./controllers/quizRoute');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// Create the uploads directory if it doesn't exist
-const fs = require("fs");
-const dir = path.join(__dirname, "uploads");
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-}
 
 app.use('/intro', introRoute);
 app.use('/quiz', quizRoute);
