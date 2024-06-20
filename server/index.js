c07eb4f574934ec78009568406a4531267a9da41
Ishu Mehta <mehta.ishu14@gmail.com>
2024-06-20 17:03:50 +0530

    refactor: Update IntroRoute.js to use new question-answering functionality

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ mongoose.connect(process.env.MONGO_URI);
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
@@ -24,6 +27,12 @@ app.get('/', (req, res) => {
 app.use('/intro', userRoute);
 app.use('/quiz', quizRoute);
 app.use('/sample', sampleRoute);
+
+// Serve React application for any routes not handled by the above routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.listen(port, () => {
     console.clear();
